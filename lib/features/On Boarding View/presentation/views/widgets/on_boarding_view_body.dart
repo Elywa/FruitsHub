10f66aec586c518f457731374440c134b8ee4b2b
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-04-27 21:31:29 +0300

    add dots indicator

--- lib/features/On Boarding View/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/On Boarding View/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,11 +1,27 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/features/On%20Boarding%20View/presentation/views/widgets/on_boarding_page_view.dart';
+import 'package:fruits_hub/utils/constants/app_colors.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
