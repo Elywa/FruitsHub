04e5c7d8cd76aa271587f503d5b81d7df5465b2d
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-05-05 15:04:59 +0300

    add auth repo impl

--- lib/features/auth/data/repo impl/auth_repo_impl.dart
+++ lib/features/auth/data/repo impl/auth_repo_impl.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruits_hub/core/errors/failures.dart';
 import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
 
-abstract class AuthRemoteDataSource {
+class AuthRepoImpl implements AuthRepo {
+  @override
   Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword({
     required String email,
     required String password,
-  });
+  }) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
 }
