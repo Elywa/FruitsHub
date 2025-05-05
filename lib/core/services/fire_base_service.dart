ba8480bb462e7a0f186be484551d5eab9fe6fe25
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-05-05 15:20:07 +0300

    add createUserWithEmailAndPassword firebase service

--- lib/core/services/fire_base_service.dart
+++ lib/core/services/fire_base_service.dart
@@ -1,8 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+
 class FireBaseService {
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-
-  
-
-
-
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException('The account already exists for that email.');
+      } else {
+        throw CustomException(
+          'An unknown error occurred, please try again later.',
+        );
+      }
+    } catch (e) {
+      throw CustomException(
+        'An unknown error occurred, please try again later.',
+      );
+    }
+  }
 }
