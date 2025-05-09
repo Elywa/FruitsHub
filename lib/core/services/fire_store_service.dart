668635e45d31a630ee53baf98d6302e7c931134c
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-05-09 15:47:57 +0300

    create fireStore class and implement add user data inside it

--- lib/core/services/fire_store_service.dart
+++ lib/core/services/fire_store_service.dart
@@ -1,1 +1,13 @@
-class FireStoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruits_hub/core/services/data_base_service.dart';
+
+class FireStoreService implements DataBaseService {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  @override
+  Future addUserData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    await firestore.collection(path).add(data);
+  }
+}
