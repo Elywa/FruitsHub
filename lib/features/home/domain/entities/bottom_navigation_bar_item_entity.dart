3a3662db84293ac307a62b05ccfff9d3e945869f
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-05-21 14:39:09 +0300

    add bottom navigation bar item entity

---
+++ lib/features/home/domain/entities/bottom_navigation_bar_item_entity.dart
@@ -0,0 +1,11 @@
+class BottomNavigationBarItemEntity {
+  final String title;
+  final String acticeIcon;
+  final String inActiveIcon;
+
+  BottomNavigationBarItemEntity({
+    required this.title,
+    required this.acticeIcon,
+    required this.inActiveIcon,
+  });
+}
